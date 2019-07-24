c8180389a52e2de2a249d5f07df90258be926b36
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2019-07-24 17:37:49 +0300

    Add PHP 7.1 packages

--- packer_templates/ubuntu/scripts/install_packages_oxvm.sh
+++ packer_templates/ubuntu/scripts/install_packages_oxvm.sh
@@ -5,6 +5,12 @@ export DEBIAN_FRONTEND=noninteractive
 ## - Add ppa Repository
 add-apt-repository ppa:ondrej/php -y;
 apt-get -y update;
+## - Install php 7.1
+apt-get install -y php7.1;
+## - Install PHP Packages
+apt-get install -y php7.1-cli php7.1-intl php7.1-mysql php7.1-gd php7.1-curl php7.1-xml php7.1-sqlite3 php7.1-xdebug;
+## - Install xDebug
+apt-get install -y php7.1-xdebug;
 ## - Install Extra Packages
 apt-get install -y git bash-completion;
 ## - Install MySQL Packages
