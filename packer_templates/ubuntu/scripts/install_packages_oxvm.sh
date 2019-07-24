655b20cf5b5c92defaf834e58965e0812afd784c
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2019-07-24 17:39:05 +0300

    Add PHP 7.2 packages

--- packer_templates/ubuntu/scripts/install_packages_oxvm.sh
+++ packer_templates/ubuntu/scripts/install_packages_oxvm.sh
@@ -5,6 +5,12 @@ export DEBIAN_FRONTEND=noninteractive
 ## - Add ppa Repository
 add-apt-repository ppa:ondrej/php -y;
 apt-get -y update;
+## - Install php 7.2
+apt-get install -y php7.2;
+## - Install PHP Packages
+apt-get install -y php7.2-cli php7.2-intl php7.2-mysql php7.2-gd php7.2-curl php7.2-xml php7.2-sqlite3 php7.2-xdebug;
+## - Install xDebug
+apt-get install -y php7.2-xdebug;
 ## - Install Extra Packages
 apt-get install -y git bash-completion;
 ## - Install MySQL Packages
